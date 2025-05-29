f1e225584a8b4cf24e2495d007124bffe21852a5
Threeaxap <threeaxap@mail.ru>
2025-05-29 05:08:03 +0500

    .

--- project/frontend/src/components/Footer.jsx
+++ project/frontend/src/components/Footer.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <div class='bg-black w-100% h-[2px] opacity-20 mt-[40px] mb-[2px]'></div>
-        <div class='bg-black w-100% h-[2px] opacity-20 mb-[15px]'></div>
-        <div class='p-[30px]'>
+        <div class='bg-black w-100% h-[1px] opacity-20 mt-[40px]'></div>
+        <div class='p-[30px] text-[14px]'>
             <p>Тех-поддержка: tech-support@mail.ru</p>
             <p>Обратная связь: answer@mail.ru</p>
         </div>
