8d4f6584501c009677032b148bb7ce5e08f45105
Threeaxap <threeaxap@mail.ru>
2025-06-05 05:37:52 +0500

    .

--- project/frontend/src/components/Card.jsx
+++ project/frontend/src/components/Card.jsx
@@ -1,23 +1,42 @@
 import { useState } from 'react';
 import React from 'react'
 import { data } from '../data/navigationbar';
+import Modal from '@mui/material/Modal';
 
 const Card = () => {
-  const [visible, setVisible] = useState(false);
-
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
       <div class='grid grid-cols-6 gap-[24px]'>
         {data.products.map(item =>(
-          <div onMouseEnter={() => setVisible((v) => !v)} key={item.id} class='items-end border-black p-[8px] m-[8px] w-fit rounded-[7px] relative'>
-              <img src={item.img} class='w-[300px] rounded-[4px]' alt="" />
-              <h2 class='text-[rgba(152,107,88)] my-[6px]'>{item.material}</h2>
-              <h3 class='font-bold'>{item.name}</h3>
-              <h4 class='text-[rgba(182,38,61)]'>${item.price}</h4>
-              {visible && (<div className='card_extra'>
-                <p>1234</p>
+          <div onClick={handleOpen} key={item.id}> 
+              <div class='items-end border-black p-[8px] m-[8px] w-fit rounded-[7px] relative'>
+                <img src={item.img} class='w-[300px] rounded-[4px]' alt="" />
+                <h3 class='font-bold'>{item.name}</h3>
+                <h4 class='text-[rgba(182,38,61)]'>${item.price}</h4>
               </div>
-              )}        
+              <Modal
+                open={open}
+                onClose={handleClose}
+              >
+                <div class='absolute bg-white top-1/4 left-1/4 p-[28px] rounded-[6px]'>
+                  <div class='flex'>
+                    <img  class='w-[300px] rounded-[4px]' src={item.img} alt="" />
+                    <div class='ml-[40px]'>
+                      <h2 class='text-[rgba(152,107,88)] my-[6px]'>{item.material}</h2>
+                      <h3 class='font-bold'>{item.name}</h3>
+                      <h4 class='text-[rgba(182,38,61)]'>{item.price}</h4>
+                    </div>
+                  </div>
+                  <div class='flex gap-[10px] mt-[20px]'>
+                    <div class='w-[30px] h-[30px] border-[1px] rounded-[5px] bg-black'></div>
+                    <div class='w-[30px] h-[30px] border-[1px] rounded-[5px] bg-slate-200'></div>
+                    <div class='w-[30px] h-[30px] border-[1px] rounded-[5px] bg-orange-100'></div>
+                  </div>
+                </div>
+              </Modal>      
           </div>
         ))} 
       </div>
