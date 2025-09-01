cb90d8bed20be016ad55f6366f7d6c15f93ceaec
Threeaxap <threeaxap@mail.ru>
2025-09-01 16:45:29 +0500

    ы

--- shop_pr/backend_test2/shop/underwear/product_underwear_unit/models.py
+++ shop_pr/backend_test2/shop/underwear/product_underwear_unit/models.py
@@ -5,5 +5,6 @@ from underwear.product_underwear_size.models import *
 class ProductUnderwearUnit(models.Model):
     variant = models.ForeignKey(ProductUnderwearVariant, on_delete=models.CASCADE)
     size = models.ForeignKey(ProductUnderwearSize, on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField(default=0)
 
     
--- shop_pr/frontend/src/components/Test/product_comp.jsx
+++ shop_pr/frontend/src/components/Test/product_comp.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Modal from '@mui/material/Modal';
 
 
 const Products = () => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const [isOpen, setIsOpen] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null)
+    const handleOpen = (id) => setSelectedProduct(id);
+    const handleClose = () => setSelectedProduct(null);
 
     useEffect(()=>{
         axios.get('http://127.0.0.1:8000/api/bra/product_bra_unit/')
@@ -25,51 +25,24 @@ const Products = () => {
   return (
       <div class='grid grid-cols-6 gap-[24px]'>
         {products.map(item =>(
-          <div onClick={handleOpen} key={item.id}> 
+          <div onClick={() => handleOpen(item?.product_cup_size_variant?.product_variant?.product_model?.id)} key={item.product_cup_size_variant.product_variant.product_model.id}> 
               <div class='items-end border-black p-[8px] m-[8px] w-fit rounded-[7px] relative'>
                 {/* <img src={item.variant.product.image} class='w-[300px] rounded-[4px]' alt="" /> */}
-                <h3 class='font-bold'>{item.variant.product.name}</h3>
-                <h4 class='text-[rgba(182,38,61)]'>${item.variant.product.price}</h4>
+                <h3 class='font-bold'>{item?.product_cup_size_variant?.product_variant?.product_model?.name}</h3>
+                <h4 class='text-[rgba(182,38,61)]'>${item?.product_cup_size_variant?.product_variant.product_model?.price}</h4>
               </div>
               <Modal
-                open={open}
+                open={selectedProduct === item?.product_cup_size_variant?.product_variant?.product_model?.id}
                 onClose={handleClose}
               >
-                <div class='absolute bg-white top-1/4 left-1/4 p-[28px] rounded-[6px]'>
-                  <div class='flex'>
-                    {/* <img  class='w-[300px] rounded-[4px]' src={item.variant.product.image} alt="" /> */}
-                    <div class='ml-[40px]'>
-                      <h2 class='text-black my-[6px]'>{item.variant.product.name}</h2>
-                      <div class='flex gap-[10px] mt-[20px]'>
-                        <div class='w-[30px] h-[30px] border-[1px] rounded-[5px] bg-black'></div>
-                        <div class='w-[30px] h-[30px] border-[1px] rounded-[5px] bg-slate-200'></div>
-                        <div class='w-[30px] h-[30px] border-[1px] rounded-[5px] bg-orange-100'></div>
-                      </div>
-                      <div class='mt-[10px] mb-[40px]'>
-                          <h3>Размеры:</h3>
-                          <div 
-                            class="border-[1px] absolute z-3 bg-white w-[100px]"
-                            onMouseEnter={() => setIsOpen(true)}
-                            onMouseLeave={() => setIsOpen(false)}
-                          >
-                            <button className="">Меню</button>
-                            
-                            {isOpen && (
-                              <div className="drop-scroll">
-                                {products.map(item =>(
-                                  <h3 key={item.id}>{item.size}</h3>
-                                ))}
-                              </div>
-                            )}
-                          </div>                         
-                      </div>
-                      <h4 class='text-[22px] mb-[10px]'>{item.price}</h4>
-                      <button className='order_but'>
-                        ДОБАВИТЬ В КОРЗИНУ
-                      </button>
-                    </div>
-                  </div>
-                </div>
+                <div class='w-200[px] h-200[px] bg-white'>
+                  {products.filter(unit =>(unit?.product_cup_size_variant?.product_variant?.product_model?.id == selectedProduct)).map(unit =>(
+                  <h3 key={unit?.product_cup_size_variant.id}>
+                    {unit?.product_cup_size_variant.product_cup_size.size}
+                  </h3>
+                ))                
+                }
+                </div>        
               </Modal>      
           </div>
         ))} 
