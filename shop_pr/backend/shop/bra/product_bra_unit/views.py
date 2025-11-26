b5a69d7305e12d0c35097843b4e55862cb605b65
Threeaxap <threeaxap@mail.ru>
2025-11-26 22:16:32 +0500

    s

--- shop_pr/backend/shop/bra/product_bra_unit/views.py
+++ shop_pr/backend/shop/bra/product_bra_unit/views.py
@@ -2,8 +2,11 @@ from .serializer import *
 from .models import *
 from rest_framework import viewsets, filters
 
-
 class ProductBraUnitViewset(viewsets.ModelViewSet):
     queryset = ProductBraUnit.objects.all()
     serializer_class = ProductBraUnitSerializer
-    
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = [
+        'size__size',
+        'cup_size__size'
+    ]
