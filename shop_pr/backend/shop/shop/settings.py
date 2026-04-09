589fbf80ba1164b710ddfa04057e696d524aedd3
Threeaxap <threeaxap@mail.ru>
2026-04-09 12:23:11 +0500

    Update settings.py

--- shop_pr/backend/shop/shop/settings.py
+++ shop_pr/backend/shop/shop/settings.py
@@ -111,22 +111,10 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'shop',
-        'USER':'postgres',
-        # 'PASSWORD':'12345',
-        'PASSWORD':'Swedxzaqws',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-    
 }
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
